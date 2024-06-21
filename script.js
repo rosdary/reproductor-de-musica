a13891d0a054e9df9cceeba468a7b9196c0be0c5
rosdary <rosdaryc@gmail.com>
2024-06-21 13:40:42 -0400

    guarde vectores

---
+++ script.js
@@ -0,0 +1,29 @@
+const tipoNumber = 2024
+const tipoString = 'instituto alfa carabobo'
+const tipoBoolean =false
+
+const tipoUndefined = undefined
+const tipoNull = null
+const tipoArray = [
+    'Leonardo',
+    'Angel',
+    'jesus',
+    'Grecia',
+    'Lineth',
+    'Rosdary',
+    'Carlos',
+]
+
+const tipoObjeto = {
+    raza: 'kacri',
+    edad: 15,
+    dueño: 'Carlos',
+    hambre: false,
+}
+
+const delivery = (song)=> {
+    //el codigo va aqui
+    document.write(song)
+
+    return song
+}
